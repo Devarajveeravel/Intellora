3f872f012cd0c190ca92957d8c8336039da79ae6
DevarajDataScientist <devarajveeravel@gmail.com>
2026-04-20 14:08:49 +0530

    fix: removed pytesseract for production

--- app/services/file_parser.py
+++ app/services/file_parser.py
@@ -1,16 +1,16 @@
-import io
-import pytesseract
-from PIL import Image
-from PyPDF2 import PdfReader
+import PyPDF2
 
+# ✅ PDF WORKS
 def extract_text_from_pdf(file_bytes):
-    pdf = PdfReader(io.BytesIO(file_bytes))
-    text = ""
-    for page in pdf.pages:
-        text += page.extract_text() or ""
-    return text
+    try:
+        reader = PyPDF2.PdfReader(file_bytes)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+        return text
+    except Exception as e:
+        return "Error reading PDF"
 
 
 def extract_text_from_image(file_bytes):
-    image = Image.open(io.BytesIO(file_bytes))
-    return pytesseract.image_to_string(image)
+    return "⚠️ Image OCR not supported in live version"
